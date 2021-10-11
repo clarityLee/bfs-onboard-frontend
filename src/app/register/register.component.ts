f40cc805dd25541bdbd7b0ca0dac83a0c0e163e4
Xian Zhang <xianzhang9825@gmail.com>
2021-10-11 16:48:31 -0400

    commented out auth service so registration html would load

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../_services/auth.service';
+//import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -15,24 +15,24 @@ export class RegisterComponent implements OnInit {
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
-
-  constructor(private authService: AuthService) {}
+  //private authService: AuthService
+  constructor() {}
 
   ngOnInit(): void {}
 
   onSubmit(): void {
     const { username, email, password } = this.form;
 
-    this.authService.register(username, email, password).subscribe(
-      (data) => {
-        console.log(data);
-        this.isSuccessful = true;
-        this.isSignUpFailed = false;
-      },
-      (err) => {
-        this.errorMessage = err.error.message;
-        this.isSignUpFailed = true;
-      }
-    );
+    // this.authService.register(username, email, password).subscribe(
+    //   (data) => {
+    //     console.log(data);
+    //     this.isSuccessful = true;
+    //     this.isSignUpFailed = false;
+    //   },
+    //   (err) => {
+    //     this.errorMessage = err.error.message;
+    //     this.isSignUpFailed = true;
+    //   }
+    //);
   }
 }
